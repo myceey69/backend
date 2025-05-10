bdcf31c3bd9015522d2505c49631f8597cf1480d
myceey69 <126042972+myceey69@users.noreply.github.com>
2025-05-10 02:13:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # Import the CORS module
 
 app = Flask(__name__)
+CORS(app)  # This allows all domains to access your API
 
-@app.route('/todos/1', methods=['GET'])
+@app.route('/todos/1')
 def get_todo():
-    todo = {
-        "userId": 1423423432,
-        "id": 1123,
-        "title": "delectus aut autem",
-        "completed": False
-    }
-    return jsonify(todo)
+    return jsonify({
+        "userId": 1,
+        "id": 1,
+        "title": "Sample Todo"
+    })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
